81ba48d829b1289349488d002cca46d0fecb802f
AlexHuynh23 <alex.huynh.23@bishops.com>
2022-09-26 08:35:18 -0700

    Initial Commit

---
+++ rotoscope.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy
+import argparse
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
